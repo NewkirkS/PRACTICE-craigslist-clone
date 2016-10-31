bbd2228384974f3abf085204c89582d601b285c4
Angie Smith and Stephen Newkirk <stephen@>
2016-10-31 09:57:41 -0700

    Create models for listings and categories

--- app/models/listing.js
+++ app/models/listing.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  title: DS.attr(),
+  price: DS.attr('number'),
+  description: DS.attr(),
+  image: DS.attr(),
+  timestamp: DS.attr(),
+  category: DS.belongsTo('category', {async: true})
 });
